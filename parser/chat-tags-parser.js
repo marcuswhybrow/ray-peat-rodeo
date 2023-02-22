823394854c5197346bb62150f6eb24d3e58365ed
Marcus Whybrow <marcus@whybrow.uk>
2023-02-22 19:13:07 +0000

    Add DOI tagging + minor refactor of parser

--- parser/chat-tags-parser.js
+++ parser/chat-tags-parser.js
@@ -1,60 +1,81 @@
 const { Parser } = require('simple-text-parser');
 const yaml = require("js-yaml");
 const fs = require("fs");
-const getLibGenSearchURI = (query) => 'https://libgen.is/search.php?req=' + encodeURIComponent(query);
-const getSciHubSearchURI = (query) => 'https://sci-hub.ru/' + encodeURIComponent(query);
-const getGoogleSearchURI = (query) => 'https://www.google.com/search?q=' + encodeURIComponent(query);
+const getLibGenSearchURL = (query) => 'https://libgen.is/search.php?req=' + encodeURIComponent(query);
+const getSciHubSearchURL = (query) => 'https://sci-hub.ru/' + encodeURIComponent(query);
+const getGoogleSearchURL = (query) => 'https://www.google.com/search?q=' + encodeURIComponent(query);
 
 const people = yaml.load(fs.readFileSync('./src/_data/people.yml', 'utf8'));
+const dois = yaml.load(fs.readFileSync('./src/_data/doi.yml', 'utf8'));
 
 class Person {
   constructor(name) {
     this.name = name;
     const searchExternallyAs = people[this.name] ? people[this.name].searchExternallyAs || this.name : this.name;
-    this.libGenURI = getLibGenSearchURI(searchExternallyAs);
-    this.googleURI = getGoogleSearchURI(searchExternallyAs);
+    this.libGenURL = getLibGenSearchURL(searchExternallyAs);
+    this.googleURL = getGoogleSearchURL(searchExternallyAs);
   }
 };
 
 class Book {
   constructor(isbn) {
     this.isbn = isbn;
-    this.libGenURI = getLibGenSearchURI(this.isbn);
-    this.googleURI = getGoogleSearchURI(this.isbn);
+    this.libGenURL = getLibGenSearchURL(this.isbn);
+    this.googleURL = getGoogleSearchURL(this.isbn);
   }
 }
 
+class SciencePaper {
+  constructor(doi) {
+    this.doi = doi;
+    this.sciHubURL = getSciHubSearchURL(this.doi);
+    this.url = dois[this.doi] ? dois[this.doi].url || this.sciHubURL : this.sciHubURL;
+  }
+}
+
+const computeNode = (pipeAndDisplayText, fallback, f) => {
+  if (pipeAndDisplayText) {
+    if (pipeAndDisplayText.length >= 1) return f(pipeAndDisplayText.substring(1) || fallback);
+    return { type: "text", text: "" };
+  }
+  return f(fallback);
+}
+
 const chatTagsParser = new Parser();
-chatTagsParser.addRule(/\[\[(\d{13}|\d{10})\|(.*?)\]\]/gi, (tag, isbn, displayText) => {
+chatTagsParser.addRule(/\[\[(\|.*)\]\]/gi, (tag, pipeAndDisplayText) => computeNode(pipeAndDisplayText, "", (displayText) => ({
+  type: "internal-link-broken",
+  text: displayText
+})));
+chatTagsParser.addRule(/\[\[(\d{13}|\d{10})(\|.*)?\]\]/gi, (tag, isbn, pipeAndDisplayText) => {
   const book = new Book(isbn);
-  return {
+  return computeNode(pipeAndDisplayText, isbn, (displayText) => ({
     type: "book",
-    text: `<a href="${book.libGenURI}" target="_blank" class="isbn">${displayText}</a>`,
+    text: `<a href="${book.libGenURL}" target="_blank" class="isbn">${displayText}</a>`,
     value: book
-  }
+  }));
 });
-chatTagsParser.addRule(/\[\[(https?:\/\/.*?)(\|(.*?))?\]\]/gi, (tag, url, displayText, displayTextSanitised) => {
-  if (displayText && !displayTextSanitised) {
-    // Specifying a blank display text ([[Name|]]) implies an invisible reference.
-    return { type: "text", text: "" };
-  }
-  return {
+chatTagsParser.addRule(/\[\[doi\:(.*?)(\|.*?)?\]\]/gi, (tag, doi, pipeAndDisplayText) => {
+  const sciencePaper = new SciencePaper(doi);
+  return computeNode(pipeAndDisplayText, sciencePaper.url, (displayText) => ({
+    type: "science-paper",
+    text: `<a href="${sciencePaper.url}" target="_blank" class="science-paper">${displayText}</a>`,
+    value: sciencePaper
+  }));
+});
+chatTagsParser.addRule(/\[\[(https?\:\/\/.*?)(\|.*)?\]\]/gi, (tag, url, pipeAndDisplayText) => {
+  return computeNode(pipeAndDisplayText, url, (displayText) => ({
     type: "external-link",
-    text: `<a href="${url}" target="_blank" class="external">${displayText ? displayTextSanitised : url}</a>`,
+    text: `<a href="${url}" target="_blank" class="external">${displayText}</a>`,
     value: url
-  }
+  }));
 });
-chatTagsParser.addRule(/\[\[(([^\|\]]*?)(?:['’]s?)?)(\|(.*?))?\]\]/gi, (tag, nameAsWritten, nameWithoutPluralisation, displayText, displayTextSanitised) => {
-  if (displayText && !displayTextSanitised) {
-    // Specifying a blank display text ([[Name|]]) implies an invisible reference.
-    return { type: "text", text: "" };
-  }
+chatTagsParser.addRule(/\[\[(([^\|\]]*?)(?:['’]s?)?)(\|(.*?))?\]\]/gi, (tag, nameAsWritten, nameWithoutPluralisation, pipeAndDisplayText) => {
   const person = new Person(nameWithoutPluralisation);
-  return {
+  return computeNode(pipeAndDisplayText, nameAsWritten, (displayText) => ({
     type: "person",
-    text: `<a href="${person.libGenURI}" target="_blank" class="person">${displayTextSanitised || nameAsWritten}</a>`,
+    text: `<a href="${person.libGenURL}" target="_blank" class="person">${displayText}</a>`,
     value: person
-  }
+  }));
 });
 
 module.exports = chatTagsParser;
