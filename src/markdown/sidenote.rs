e342c3761a700ee1b046a487c7b6a30eb741b808
Marcus Whybrow <marcus@whybrow.uk>
2023-09-09 11:58:09 +0100

    Fix non-UTF-8 preceding chars breaking sidenote
    Also, refactored sidenote logic to jibe better with mention logic

--- src/markdown/sidenote.rs
+++ src/markdown/sidenote.rs
@@ -42,54 +42,43 @@ impl InlineRule for SidenodeInlineScanner {
     const MARKER: char = '{';
 
     fn run(state: &mut InlineState) -> Option<(Node, usize)> {
-        if state.level > 0 { return None } // (nested) inside img or link
+        if state.level > 0 || state.src.get(state.pos..state.pos+1)? != "{" {
+            return None;
+        }
 
         let starting_pos = state.pos;
-
-        let Some(char) = state.src.chars().nth(state.pos) else { return None };
-        if char != '{' { return None };
-
         state.pos += 1;
 
         while state.pos < state.pos_max {
-            let char = {
-                state.md.inline.skip_token(state); // increases state.pos to overcome the next token 
+            // Increase state.pos to overcome the next token.
+            // This prevents finding a closing "}" that matches a nested rule
+            state.md.inline.skip_token(state);
 
-                if state.pos >= state.pos_max { return None }
-
-                match state.src.chars().nth(state.pos) {
-                    Some(char) => char,
-                    None => return None,
-                }
-            };
-
-            if char != '}' { continue }
-
-            if let Some(next_char) = state.src.chars().nth(state.pos + 1) {
-                if !next_char.is_whitespace() { return None }
+            // Keep skipping tokens until we find the closing "}"
+            if state.src.get(state.pos..state.pos+1)? != "}" {
+                continue;
             }
 
             let consumed = state.pos + 1 - starting_pos;
 
-            // Inspiration: https://github.com/rlidwka/markdown-it.rs/blob/eb5459039685d19cefd0361859422118d08d35d4/src/generics/inline/full_link.rs#L124-L136
+            // Create a new InlineSidenote node, and use the existing parser 
+            // state to tokenize the body.
             let node = {
-                let original_node = std::mem::replace(&mut state.node, Node::new(InlineSidenote {
+                let node = std::mem::replace(&mut state.node, Node::new(InlineSidenote {
                     position: {
                         let position = state.root_ext.get_or_insert(Position(0));
                         position.0 += 1;
                         position.0
                     }
                 }));
+                let pos = std::mem::replace(&mut state.pos, starting_pos + 1);
+                let pos_max = std::mem::replace(&mut state.pos_max, pos);
 
-                let original_pos_max = state.pos_max;
-                state.pos_max = state.pos;
-                state.pos = starting_pos + 1;
                 state.md.inline.tokenize(state);
 
                 state.pos = starting_pos;
-                state.pos_max = original_pos_max;
-
-                std::mem::replace(&mut state.node, original_node)
+                state.pos_max = pos_max;
+                std::mem::replace(&mut state.node, node)
             };
 
             return Some((node, consumed));
