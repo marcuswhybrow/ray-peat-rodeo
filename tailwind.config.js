8b2babe122cdd201993c2de7bb159b174c316d17
Marcus Whybrow <marcus@whybrow.uk>
2023-11-11 10:45:01 +0000

    Correct tailwind.config.js code in comment docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,13 @@ module.exports = {
     // Scanning source code is an order of magnitude faster than scanning build. 
     // But we cannot use runtime values in class names. For example,
     // 
-    // # Tailwind cannot understand this at compile time
+    // ```
+    // // Tailwind cannot understand this at compile time
     // fmt.Sprintf(`<span class="before:content-['%v']">`, count)
     //
-    // # So far, CSS counters have sufficed in such situations:
+    // // So far, CSS counters have sufficed in such situations:
     // `<span class="[counter-increment:count] before:content-[counter(count)]">`
+    // ```
     './cmd/**/*.{templ,go}',
     './internal/**/*.{templ,go}',
   ],
