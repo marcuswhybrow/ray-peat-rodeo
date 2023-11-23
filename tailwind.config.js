079775c31d171a82b7d4afb55111254f850c3263
Marcus Whybrow <marcus@whybrow.uk>
2023-11-23 17:25:42 +0000

    Add HayesKim to new supporters section on homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './build/**/*.html',
+    './cmd/ray-peat-rodeo/**/*.{templ,go}',
+    './internal/**/*.{templ,go}',
+    './build/index.html'
   ],
   theme: {
     extend: {
