fdd6147cc131ea83ae6f10e189f27c91edce9ee0
Marcus Whybrow <marcus@whybrow.uk>
2023-11-23 03:30:23 +0000

    Fix missing tailwind classes for mention popups

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Scanning source code is an order of magnitude faster than scanning build. 
-    // But we cannot use runtime values in class names. For example,
-    // 
-    // ```
-    // // Tailwind cannot understand this at compile time
-    // fmt.Sprintf(`<span class="before:content-['%v']">`, count)
-    //
-    // // So far, CSS counters have sufficed in such situations:
-    // `<span class="[counter-increment:count] before:content-[counter(count)]">`
-    // ```
-    './build/index.html',
-    './build/2018-01-22-when-western-medicine-isn\'t-working/index.html',
-    './build/2022-11-18-untitled-interview-2022-11-18/index.html'
+    './build/**/*.html',
   ],
   theme: {
     extend: {
