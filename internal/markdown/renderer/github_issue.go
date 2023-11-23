587ede8680adf8c49372f5c3f3ad9e39bbbcf109
Marcus Whybrow <marcus@whybrow.uk>
2023-11-23 18:59:06 +0000

    Fix all issue element IDs missing

--- internal/markdown/renderer/github_issue.go
+++ internal/markdown/renderer/github_issue.go
@@ -25,7 +25,9 @@ func (t *GitHubIssueHTMLRendereer) renderGitHubIssue(w util.BufWriter, source []
 		github_issue := node.(*ast.GitHubIssue)
 
 		t, err := template.New("openGitHubIssue").Parse(`
-      <a href="{{ .url }}"
+      <a 
+        id="issue-{{ .id }}"
+        href="{{ .url }}"
         class="z-10 block transition-all m-2 p-4 hover:translate-y-1 shadow-xl hover:shadow-2xl shadow-yellow-800/20 hover:shadow-yellow-600/40 rounded-md bg-gradient-to-br from-yellow-200 from-10% to-amber-200 hover:from-yellow-100 hover:from-70% hover:to-amber-200 xl:block w-2/5 mr-[-5%] md:mr-[-10%] lg:mr-[-20%] float-right clear-right text-sm relative leading-5 tracking-tight">
 
         <span class="text-yellow-900 font-bold mr-0.5">
