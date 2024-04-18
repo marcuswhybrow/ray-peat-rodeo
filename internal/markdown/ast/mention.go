a20479a527efa1084ee4959369124bef0e7cb1df
Marcus Whybrow <marcus@whybrow.uk>
2024-04-18 19:41:50 +0100

    Add comments to mention AST code

--- internal/markdown/ast/mention.go
+++ internal/markdown/ast/mention.go
@@ -11,6 +11,7 @@ import (
 
 var countKey = gparser.NewContextKey()
 
+// Represents a mention in the markdown parser's Abstract Syntax Tree.
 type Mention struct {
 	BaseInline
 	FileNode
@@ -37,12 +38,14 @@ type Mention struct {
 	MentionableSource Source
 }
 
+// A location in a source markdown file
 type Source struct {
 	Row     int
 	Col     int
 	Segment text.Segment
 }
 
+// Creates a new Mention with the correct context
 func NewMention(pc gparser.Context, mentionable Mentionable, label string) *Mention {
 	count := pc.ComputeIfAbsent(countKey, func() interface{} {
 		return map[Mentionable]int{}
@@ -62,6 +65,10 @@ func NewMention(pc gparser.Context, mentionable Mentionable, label string) *Ment
 	}
 }
 
+// Computes a ID for this mention that's unique to this document.
+// Because a single mentionable maybe mentioned multiple times,
+// the local ID takes the number of occurances of a particular
+// mentionable into account, in order to compute a unique local ID.
 func (m *Mention) LocalID() string {
 	id := m.Mentionable.ID()
 	if m.Occurance > 1 {
@@ -70,14 +77,19 @@ func (m *Mention) LocalID() string {
 	return id
 }
 
+// A globally unique ID for this mention in this file.
 func (m *Mention) ID() string {
 	return m.LocalID() + "@" + m.File.GetID()
 }
 
+// A URL that links directly to this mention using an HTTP fragment.
 func (m *Mention) Permalink() string {
 	return m.File.GetPermalink() + "#" + m.LocalID()
 }
 
+// Returns a mention in plain text (formatting removed), surrounded by a
+// specified amount of adjacent, contextual text.
+// The mentioned text is wraped in a <mark> HTML tag with baked in styling.
 func (m *Mention) VignetteHTML(source []byte, radius int) string {
 	for p := m.Parent(); p != nil; p = p.Parent() {
 		if p.Kind() == gast.KindParagraph {
@@ -111,10 +123,12 @@ func (m *Mention) VignetteHTML(source []byte, radius int) string {
 	panic("Failed to find parent utterance block for mention node")
 }
 
+// Required to be used as a node in the Markdown AST
 func (m *Mention) Dump(source []byte, level int) {
 	gast.DumpHelper(m, source, level, nil, nil)
 }
 
+// Returns the text the human reader will see.
 func (m *Mention) Text(source []byte) []byte {
 	if len(m.Label) > 0 {
 		return []byte(m.Label)
@@ -123,14 +137,30 @@ func (m *Mention) Text(source []byte) []byte {
 	}
 }
 
+// The Markdown Abstract Syntax Tree Node Kind is a way of
+// hooking our Mention class into the Markdown parser
 var KindMention = gast.NewNodeKind("Mention")
 
+// Tells the Markdown parser which Node Kind a Mention is
 func (m *Mention) Kind() gast.NodeKind {
 	return KindMention
 }
 
-// Recursive search of tree for a target Node that returns the Node text before
-// and after the target.
+// Recursive search of Abstracy Syntax Tree for a target Node.
+// returns the Node text before and after the target.
+//
+// The source argument is the original markdown input string. Each AST Node
+// stores a reference to a segment from this source, i.e. it doesn't have it's
+// own copy. Hence we need the original source as an argument to determine an
+// arbitary node's final text: the bit the a human reads.
+//
+// The root argument is any AST node that has children to search.
+//
+// The target argument will be equated against each child to find a match.
+//
+// The found argument should be passed as `false` in the initial call.
+// This is a recursive function. Once the target is located, found is
+// set to true so later iterations can act appropriately.
 func cutText(source []byte, root gast.Node, target gast.Node, found bool) (string, string, bool) {
 
 	if root.HasChildren() {
